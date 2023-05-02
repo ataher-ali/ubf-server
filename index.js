938adeaa2744ce0a40c8ab40e63c16cffc174636
ataher-ali <ataher.dev@gmail.com>
2023-05-02 12:21:19 +0600

    chef api rady

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 1010
+const Chefs = require('./chef.json')
 
 app.get('/', (req, res) => {
-  res.send('Welcome to  🍕🍜 restaurant 📡 server ! ') 
+  res.send('Welcome to  🍕🍜rest 2 rant 📡 server ! ') 
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(Chefs)
+  })
+
+  app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(`params ${id}`);
+    const Chef = Chefs.find(chef=>chef._id===id )||{message: "data not found!"};
+    // if(job){
+    //     res.send(job)
+    // }
+    // else{
+    //     res.json({message: "data not found!"})
+    // }
+    res.send(Chef) // if if-else statement will you work 23 line make salient
 })
 
 app.listen(port, () => {
